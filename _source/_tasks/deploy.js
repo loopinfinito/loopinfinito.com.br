c4de184f8fd60e7bcecc1c5fc47ad0bcd3e4396e
Caio Gondim <me@caiogondim.com>
2013-02-24 23:07:38 -0300

    Grunt carrega tasks locais definidas na pasta '_source/_tasks'

---
+++ _source/_tasks/deploy.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+	grunt.registerTask('deploy', 'Envia o diff do blog para o server de staging', function(ambiente) {
+		var spawn = require('child_process').spawn
+		var exec = require('child_process').exec
+
+		var done = this.async()
+
+		// configurações de deploy do rsync
+		// para poder dar o deploy com sucesso, é necessário que sua chave
+		// pública esteja no arquivo ~/.ssh/authorized_keys do servidor
+		var user = 'loopinfinito'
+		var localRoot = '../../site/'
+		if (ambiente == 'staging') {
+			var blogUrl = 'staging.loopinfinito.com.br'
+			var remoteRoot = '~/' + blogUrl + '/'
+		} else if(!ambiente) {
+			var blogUrl = 'loopinfinito.com.br'
+			var remoteRoot = '~/' + blogUrl + '/'
+		}
+
+		grunt.log.writeln('Upando arquivos...')
+		var rsync = spawn('rsync', [
+			'-avz',
+			'--stats',
+			'-e',
+			'ssh',
+			__dirname + '/' + localRoot,
+			user + '@bugsy.dreamhost.com:' + remoteRoot
+		])
+
+		// evento disparado quando a tarefa imprime algo no stdout
+		rsync.stdout.on('data', function(data) {
+			// grunt.log.writeln(data.toString().trim())
+		})
+
+		// evento disparado caso ocorra um erro na tarefa
+		rsync.stderr.on('data', function(data) {
+			grunt.log.errorlns('Erro no deploy: ' + data)
+		})
+
+		// evento disparado quando a tarefa é terminada
+		rsync.on('exit', function(code) {
+			grunt.log.ok(blogUrl + ' atualizado')
+			exec('open http://' + blogUrl)
+			done(true)
+		})
+	})
+}
