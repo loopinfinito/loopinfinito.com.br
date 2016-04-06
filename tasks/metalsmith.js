08891c0782f0bb32022b84304f2024a42cc0457a
Almir Filho <almirflorenciofilho@gmail.com>
2016-04-06 20:37:56 -0300

    Adds content watching (closes #308)

--- tasks/metalsmith.js
+++ tasks/metalsmith.js
@@ -1,6 +1,7 @@
 const config = require('../package.json').config;
 const metalsmith = require('metalsmith');
 const markdown = require('metalsmith-markdown');
+const watch = require('metalsmith-watch');
 
 
 module.exports = workingdir => {
@@ -8,10 +9,15 @@ module.exports = workingdir => {
     .source(config.contentDir)
     .use(markdown());
 
+  const buildCallback = (err, files) => {
+    if(err) throw err;
+    else console.log('DONE OK 🚀')
+  };
+
   return {
-    build: pipeline.build.bind(pipeline, (err, files) => {
-      if(err) throw err;
-      else console.log('DONE OK 🚀')
-    })
+    build: () => pipeline.build(buildCallback),
+    watch: () => pipeline
+      .use(watch(config.metalsmith.watch))
+      .build(buildCallback)
   }
 };
