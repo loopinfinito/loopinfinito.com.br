a3cf660e744dacd24a93ef2a8f629b2dece2df26
Almir Filho <almirflorenciofilho@gmail.com>
2016-04-07 11:07:36 -0300

    Configures templating with nunjucks

--- tasks/metalsmith.js
+++ tasks/metalsmith.js
@@ -1,23 +1,25 @@
 const config = require('../package.json').config;
 const metalsmith = require('metalsmith');
 const markdown = require('metalsmith-markdown');
+const layouts = require('metalsmith-layouts');
 const watch = require('metalsmith-watch');
 
 
 module.exports = workingdir => {
   const pipeline = metalsmith(workingdir)
     .source(config.contentDir)
-    .use(markdown());
+    .use(markdown())
+    .use(layouts(config.metalsmith.layouts));
 
-  const buildCallback = (err, files) => {
+  const afterBuild = (err, files) => {
     if(err) throw err;
     else console.log('DONE OK 🚀')
   };
 
   return {
-    build: () => pipeline.build(buildCallback),
+    build: () => pipeline.build(afterBuild),
     watch: () => pipeline
       .use(watch(config.metalsmith.watch))
-      .build(buildCallback)
+      .build(afterBuild)
   }
 };
